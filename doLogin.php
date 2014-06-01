80a1a5cf920b16821c23e9e2e36b107f582d7838
Jakousa <japekohatcher93@hotmail.com>
2014-06-01 17:44:55 +0300

    kirjautumisharjoitus

--- doLogin.php
+++ doLogin.php
@@ -1,1 +1,19 @@
-<?php ?>
+<?php
+  
+  if (empty($_POST["username"]) || empty($_POST["password"])) {
+     /* Käytetään omassa kirjastotiedostossa määriteltyä näkymännäyttöfunktioita */
+    naytaNakyma("login");
+    exit(); // Lopetetaan suoritus tähän. Kutsun voi sijoittaa myös naytaNakyma-funktioon, niin sitä ei tarvitse toistaa joka paikassa
+  }
+
+  $kayttaja = $_POST["username"];
+  $salasana = $_POST["password"];
+  
+  /* Tarkistetaan onko parametrina saatu oikeat tunnukset */
+  if ("svinhufvud" == $kayttaja && "kissakartano" == $salasana) {
+    /* Jos tunnus on oikea, ohjataan käyttäjä sopivalla HTTP-otsakkeella kissalistaan. */
+    header('Location: kissalista.php');
+  } else {
+    /* Väärän tunnuksen syöttänyt saa eteensä kirjautumislomakkeen. */
+    naytaNakyma("login");
+  }
--- views/login.php
+++ views/login.php
@@ -1,44 +1,32 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link href="../css/bootstrap.css" rel="stylesheet">
-        <link href="../css/bootstrap-theme.css" rel="stylesheet">
-        <link href="../css/main.css" rel="stylesheet">
-        <title></title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    </head>
-    <body>
-        <div class="container">
-            <h1>Login</h1>
-            <form class="form-horizontal" role="form" action="login.php" method="POST">
-                <div class="form-group">
-                    <label for="inputEmail" class="col-md-2 control-label">Username</label>
-                    <div class="col-md-10">
-                        <input type="email" class="form-control" id="inputEmail1" name="tunnus" placeholder="Username">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputPassword" class="col-md-2 control-label">Salasana</label>
-                    <div class="col-md-10">
-                        <input type="password" class="form-control" id="inputPassword1" name="password" placeholder="Password">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-md-offset-2 col-md-10">
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox"> Remember login
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-md-offset-2 col-md-10">
-                        <button type="submit" class="btn btn-default">Login</button>
-                        <button type="submit" class="btn btn-default">Cancel</button>
-                    </div>
-                </div>
-            </form>
+<div class="container">
+    <h1>Login</h1>
+    <form class="form-horizontal" role="form" action="login.php" method="POST">
+        <div class="form-group">
+            <label for="inputEmail" class="col-md-2 control-label">Username</label>
+            <div class="col-md-10">
+                <input type="email" class="form-control" id="inputEmail1" name="tunnus" placeholder="Username">
+            </div>
         </div>
-    </body>
-</html>
+        <div class="form-group">
+            <label for="inputPassword" class="col-md-2 control-label">Salasana</label>
+            <div class="col-md-10">
+                <input type="password" class="form-control" id="inputPassword1" name="password" placeholder="Password">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-md-offset-2 col-md-10">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox"> Remember login
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-md-offset-2 col-md-10">
+                <button type="submit" class="btn btn-default">Login</button>
+                <button type="submit" class="btn btn-default">Cancel</button>
+            </div>
+        </div>
+    </form>
+</div>
